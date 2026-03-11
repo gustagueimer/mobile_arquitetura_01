8fd6fbb71c6502d90b9dd815ebffd9d8727b65a8
gustagueimer <gustavomarqueslima@outlook.com>
2026-03-11 10:23:48 -0300

    chore: correção de identação

--- lib/features/product/data/repositories/product_repository_impl.dart
+++ lib/features/product/data/repositories/product_repository_impl.dart
@@ -4,18 +4,19 @@ import '../datasources/product_remote_datasource.dart';
 
 class ProductRepositoryImpl implements ProductRepository {
   final ProductRemoteDatasource datasource;
-    ProductRepositoryImpl(this.datasource);
     
-    @override
-    Future<List<Product>> getProducts() async {
-      final models = await datasource.getProducts();
-      return models
-        .map((m) => Product(
-          id: m.id,
-          title: m.title,
-          price: m.price,
-          image: m.image,
-        ))
-        .toList();
-}
+  ProductRepositoryImpl(this.datasource);
+    
+  @override
+  Future<List<Product>> getProducts() async {
+    final models = await datasource.getProducts();
+    return models
+      .map((m) => Product(
+        id: m.id,
+        title: m.title,
+        price: m.price,
+        image: m.image,
+      ))
+      .toList();
+  }
 }
